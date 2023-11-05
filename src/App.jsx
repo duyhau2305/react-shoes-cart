501dded5418d3e232b7072579f071e29a8e0c013
Tony Nguyen <nhattruong1811@gmail.com>
2023-11-05 08:39:16 +0700

    total

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import ProductItem from "./pages/ProductItem";
 import CartItem from "./pages/CartItem";
 
 function App() {
+
   return (
     <div className="mainContent">
       <div className="card">
@@ -22,22 +23,7 @@ function App() {
 
       {/* cart */}
       <div className="card">
-        <div className="cardTop">
-          <img
-            alt=""
-            src="https://cdn-icons-png.flaticon.com/512/732/732084.png"
-          />
-          <div>Total: 10</div>
-        </div>
-
-        <div className="cardTitle">
-          <span>Your cart</span>
-          <span className="card_amount">$89.97</span>
-        </div>
-
-        <div className="cardBody">
-          <CartItem />
-        </div>
+        <CartItem />
       </div>
     </div>
   );
--- src/pages/CartItem.jsx
+++ src/pages/CartItem.jsx
@@ -17,30 +17,46 @@ function CartItem() {
 
   return (
     <>
-      {carts.map((cart) => (
-        <div key={cart.id} className="cardItem">
-          <div className="cardItem_left">
-            <Image className="cardItem_image" src={cart.image} />
-          </div>
-          <div className="cardItem_right">
-            <div className="cardItem_name">{cart.title}</div>
-            <div className="cardItem_price">${cart.price}</div>
-            <div className="cartItem_actions">
-              <div className="cartItem_count">
-                <div className="cartItem_button">-</div>
-                <div className="cartItem_number">1</div>
-                <div className="cartItem_button">+</div>
-              </div>
-              <div className="carItem_remove">
-                <Button
-                  icon="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
-                  onClick={() => handleRemove(cart.id)}
-                />
+      <div className="cardTop">
+        <img
+          alt=""
+          src="https://cdn-icons-png.flaticon.com/512/732/732084.png"
+        />
+        <div>Total: {carts.length}</div>
+      </div>
+
+      <div className="cardTitle">
+        <span>Your cart</span>
+        <span className="card_amount">$89.97</span>
+      </div>
+      
+       <div className="cardBody">
+        {carts.map((cart) => (
+          <div key={cart.id} className="cardItem">
+            <div className="cardItem_left">
+              <Image className="cardItem_image" src={cart.image} />
+            </div>
+            <div className="cardItem_right">
+              <div className="cardItem_name">{cart.title}</div>
+              <div className="cardItem_price">${cart.price}</div>
+              <div className="cartItem_actions">
+                <div className="cartItem_count">
+                  <div className="cartItem_button">-</div>
+                  <div className="cartItem_number">1</div>
+                  <div className="cartItem_button">+</div>
+                </div>
+                <div className="carItem_remove">
+                  <Button
+                    icon="https://cdn-icons-png.flaticon.com/512/1214/1214428.png"
+                    onClick={() => handleRemove(cart.id)}
+                  />
+                </div>
               </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+       </div>
+      
     </>
   );
 }
